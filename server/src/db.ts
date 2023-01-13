5da6df3330999dea4896b0a357543df078221433
delbaze <56109313+delbaze@users.noreply.github.com>
2023-01-13 12:38:26 +0100

    pg

--- server/src/db.ts
+++ server/src/db.ts
@@ -4,11 +4,22 @@ import Skill from "./entity/Skill";
 import Wilder from "./entity/Wilder";
 
 const datasource = new DataSource({
-  type: "sqlite",
-  database: "./wildersdb.sqlite",
+  type: "postgres",
+  host: "db",
+  port: 5432,
+  username: "postgres",
+  password: "postgres",
+  database: "wilders",
   synchronize: true,
   entities: [Wilder, Skill, Grade],
   logging: ["query", "error"],
 });
+// const datasource = new DataSource({
+//   type: "sqlite",
+//   database: "./wildersdb.sqlite",
+//   synchronize: true,
+//   entities: [Wilder, Skill, Grade],
+//   logging: ["query", "error"],
+// });
 
 export default datasource;
